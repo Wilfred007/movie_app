27bf3130203103dcd9ed4517399a53b54cb19cd8
Wilfred007 <adzerwilfred007@gmail.com>
2024-07-10 06:59:59 -0700

    bg

--- src/component/Row.jsx
+++ src/component/Row.jsx
@@ -25,12 +25,16 @@ function Row({title, fetchUrl}) {
   console.log(movies)
   return (
     <>
-    <h2 className='font-bold md:text-xl p-4'>{title}</h2>
+    <h2 className='font-bold md:text-xl p-4 text-white'>{title}</h2>
     <div className='relative flex items-center'>
       <div id={'slider'}>
         {movies.map((items, id)=>(
-          <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2'>
-            <img src={`https://image.tmdb.org/t/p/original/${items?.backdrop_path}`} alt={items.title} />
+          <div className='w-[160px] sm:w-[200px] md:w-[240px] lg:w-[280px] inline-block cursor-pointer relative p-2 '>
+            <img className='w-full h-auto block' src={`https://image.tmdb.org/t/p/original/${items?.backdrop_path}`} alt={items.title} />
+            <div className='absolute top-0 left-0 w-full h-full hover:bg-black/80 opacity-0 hover:opacity-100 text-white'>
+              <p className='white-space-normal text-xs md:text-sm font-bold flex justify-center items-center h-full text-center'>{items?.title}</p>
+
+            </div>
           </div>
         ))}
       </div>
