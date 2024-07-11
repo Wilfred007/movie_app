3f24da9d7b217b41d448672350ec2ccdbf7e36e9
Wilfred007 <adzerwilfred007@gmail.com>
2024-07-11 10:20:59 -0700

    name

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Navbar() {
   return (
     <div className='flex items-center justify-between p-4 z-[100] absolute w-full'>
-      <h1 className='text-red-600 text-2xl font-bold cursor-pointer'>MyFlix</h1>
+      <h1 className='text-red-600 text-2xl font-bold cursor-pointer'>WiFlix</h1>
       <div>
         <button className='text-red-600 pr-4'>Sign In</button>
         <button className='bg-red-600 px-5 py-1 rounded cursor-pointer text-white'>Sign Up </button>
---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyBE4w9bBbZU24-CU-JjTxX_XTrZTljmDfM",
+  authDomain: "myflix-bfbef.firebaseapp.com",
+  projectId: "myflix-bfbef",
+  storageBucket: "myflix-bfbef.appspot.com",
+  messagingSenderId: "734031294603",
+  appId: "1:734031294603:web:a09ad97920b9769578a48e"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
