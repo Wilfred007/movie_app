e5fec4ab95de7995492816ab4e78cde30e5c901a
Wilfred007 <adzerwilfred007@gmail.com>
2024-07-11 11:39:15 -0700

    update

--- src/firebase.js
+++ src/firebase.js
@@ -5,12 +5,12 @@ import { initializeApp } from "firebase/app";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBE4w9bBbZU24-CU-JjTxX_XTrZTljmDfM",
-  authDomain: "myflix-bfbef.firebaseapp.com",
-  projectId: "myflix-bfbef",
-  storageBucket: "myflix-bfbef.appspot.com",
-  messagingSenderId: "734031294603",
-  appId: "1:734031294603:web:a09ad97920b9769578a48e"
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER,
+  appId: process.env.REACT_APP_APP_ID
 };
 
 // Initialize Firebase
